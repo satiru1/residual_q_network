e2ca5acbe1efe61781829c0f176db2f10a03b19d
satiru1 <78566351+satiru1@users.noreply.github.com>
2021-02-05 00:56:22 +0000

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,4 @@
+python3 main.py --env Checkers-v0 --alg vdn -ne 20000
+python3 main.py --env PredatorPrey5x5-v0 --alg vdn -ne 20000
+python3 main.py --env Combat-v0 --alg vdn -ne 20000
+python3 main.py --env Switch2-v0 --alg vdn -ne 20000
